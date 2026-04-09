a07f6bea7a2f537d0d3ef64b338146e5a428b2f0
MikeBuckets2 <michaelmorris121@yahoo.com>
2026-04-09 15:18:03 +0000

    feat: add all 3 components to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+import Experience from './components/Experience';
+import './styles/App.css';
+
 function App() {
-  return <div>CV Generator</div>;
+  return (
+    <div>
+      <h1>CV Application</h1>
+      <GeneralInfo />
+      <Education />
+      <Experience />
+    </div>
+  );
 }
 
 export default App;
